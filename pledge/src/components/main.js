855cfa4e88f0c3942664973f8e6f0cc5d6b3d947
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-09-21 12:03:34 +0300

    Create main.js

---
+++ pledge/src/components/main.js
@@ -0,0 +1,57 @@
+import Button from 'react-bootstrap/Button'
+import React, { useState } from 'react'
+import { Container, Stack } from 'react-bootstrap'
+import PledgeCard from './PledgeCard'
+import AddPledgeModal from './AddPledgeModal'
+import { usePledges } from '../context/PledgeContext'
+import AddContributorModal from './AddContributor'
+import MakePledgePaymentModal from './MakePledgePayment'
+import TotalPledgeAmount from './TotalPledgeAmount'
+
+
+export default function Main() {
+    const [viewPledgeForm, setViewPledgeForm] = useState(false)
+
+    const [viewAddContributorForm, setViewAddContributorForm] = useState(false)
+
+    const [viewPledgePayment, setViewPledgePayment] = useState(false)
+    const [pledgeName, setPledgeName] = useState()
+
+    const { pledges, getTotalPledgePayments } = usePledges()
+
+    // const test_array = [300, 200, 1000, 1200]
+    return (
+        <>
+            <Container className='my-4'>
+                <Stack direction='horizontal' gap={2} className='mb-4'>
+                    <h1 className='me-auto'>PLEDGE MANAGEMENT</h1>
+                    <Button variant="primary" onClick={() => setViewPledgeForm(true)}> Add Pledge</Button>
+                    <Button variant="outline-primary" onClick={() => setViewAddContributorForm(true)}>Add Contributor</Button>
+                </Stack>
+
+                <div style={{ display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(400px,1fr))', gap: '1rem', alignItems: 'flex-start' }}>
+                    {pledges.map(p => {
+
+                        return (
+                            <PledgeCard pledgename={p.name} collected={getTotalPledgePayments(p.name)} target={p.target} paymentAction={() => {
+                                setPledgeName(p.name)
+                                setViewPledgePayment(true)
+                            }} />
+                        )
+
+                    })
+                    }
+                    <TotalPledgeAmount />
+                </div>
+
+            </Container>
+            <AddPledgeModal show={viewPledgeForm} handleClose={() => setViewPledgeForm(false)} />
+
+            <AddContributorModal show={viewAddContributorForm}
+                handleClose={() => setViewAddContributorForm(false)}
+            />
+
+            <MakePledgePaymentModal show={viewPledgePayment} handleClose={() => setViewPledgePayment(false)} pledgeName={pledgeName} />
+        </>
+    )
+}
